9f8693d649c43206ef033d7c9ca337cb59a13947
fflorezz <felipeflorezart@gmail.com>
2021-02-24 16:35:51 -0500

    Added posts and users to router intro

---
+++ 12-router-intro/src/components/PostCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="post-card">
+    <h2>{{ title }}</h2>
+    <br />by <br />
+    <h4>{{ userName }}</h4>
+    <p>{{ body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostCard",
+  props: ["title", "body", "userId"],
+  inject: ["users"],
+  computed: {
+    userName() {
+      const user = this.users.find((user) => {
+        return user.id === this.userId;
+      });
+      return user.name;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.post-card {
+  border: 1px solid gray;
+  width: 600px;
+  margin-top: 10px;
+  padding: 10px;
+}
+</style>
--- 12-router-intro/src/views/Posts.vue
+++ 12-router-intro/src/views/Posts.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
     <h1>POSTS PAGE</h1>
+    <ul>
+      <post-card
+        v-for="post in posts"
+        :key="post.id"
+        :title="post.title"
+        :body="post.body"
+        :userId="post.userId"
+      ></post-card>
+    </ul>
   </div>
 </template>
+
+<script>
+import PostCard from "../components/PostCard.vue";
+
+export default {
+  components: { PostCard },
+  name: "Posts",
+  inject: ["posts"],
+};
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
